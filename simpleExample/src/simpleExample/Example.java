37b3f6c42bb10bbf4ce66fcaf33e190ed4a97d66
vahid <vahid_kh_g@yahoo.com>
2016-09-24 23:22:03 +0200

    delete for

--- simpleExample/src/simpleExample/Example.java
+++ simpleExample/src/simpleExample/Example.java
@@ -22,11 +22,7 @@ public class Example {
 		
 		System.out.println("someValues da someValu es");
 		
-		for (int i = 0; i < args.length; i++) {
-			System.out.println("1 4 5 6 7 8");
-			
-			
-		}
+	
 		
 		
 	}
