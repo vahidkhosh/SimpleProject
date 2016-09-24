f97fa4950faeac7896d742efa148dfe4f1a6c4c1
vahid <vahid_kh_g@yahoo.com>
2016-09-24 23:21:27 +0200

    add a for

--- simpleExample/src/simpleExample/Example.java
+++ simpleExample/src/simpleExample/Example.java
@@ -20,9 +20,14 @@ public class Example {
 		System.out.println("change default cambio");
 		
 		
-		System.out.println("someValues da someValues");
+		System.out.println("someValues da someValu es");
 		
-		System.out.println("1 4 5 6 7 8");
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("1 4 5 6 7 8");
+			
+			
+		}
+		
 		
 	}
 
