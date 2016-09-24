14fae3514abc87ec80bccff395ff1715070319e5
vahid <vahid_kh_g@yahoo.com>
2016-09-24 23:22:35 +0200

    add a for

--- simpleExample/src/simpleExample/Example.java
+++ simpleExample/src/simpleExample/Example.java
@@ -17,7 +17,11 @@ public class Example {
 		System.out.println("cambiamento da Beny");
 		
 		
-		
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("1 4 5 6 7 8");
+			
+			
+		}
 	}
 
 }
